7b2eaff2094ea277581dc646576762f3ad57e7ae
984837 <984837@CS_LAB115-107.cs.mum.edu>
2016-02-01 14:02:05 -0600

    Signed-off-by: 984837 <984837@CS_LAB115-107.cs.mum.edu>

---
+++ src/com/mpp/assignment_javafx_sceanbuilder/Main.java
@@ -0,0 +1,14 @@
+package com.mpp.assignment_javafx_sceanbuilder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		
+		
+	}
+
+}
